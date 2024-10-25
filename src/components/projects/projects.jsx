6183e41ac4dd1cedf4a3ad5e4bf294a7bcda88a5
Edward Flores <edwardvflores1@gmail.com>
2024-10-26 01:33:46 +0300

    Add icons to links

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -9,9 +9,9 @@ const Projects = () => {
             <div className="projects-container">
                 <h3 className="projects-title">Projects</h3>
                 <div className="projects-button-container">
-                    <PrimaryButton text="Pokedex" link="https://h7ehdp.mimo.run/"/>
-                    <PrimaryButton text="Edwards Trivia" link="https://2386j7.mimo.run/"/>
-                    <PrimaryButton text="Camera Tips" link="https://jgkd0c.mimo.run/"/>
+                    <PrimaryButton text="Pokedex" link="https://h7ehdp.mimo.run/" buttonClass={"fa-solid fa-gamepad"}/>
+                    <PrimaryButton text="Edwards Trivia" link="https://2386j7.mimo.run/" buttonClass={"fa-solid fa-clipboard-question"}/>
+                    <PrimaryButton text="Camera Tips" link="https://jgkd0c.mimo.run/" buttonClass={"fa-solid fa-camera"}/>
                 </div>
             </div>
         </div>
